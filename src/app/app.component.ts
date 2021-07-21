bc29b23730774d9b629bd098a32c0d93d3982a3f
renso <arcadu22@hotmail.com>
2021-07-21 18:38:51 -0500

    creacion de servicio cliente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClienteService } from './services/cliente/cliente.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'vuelos';
+export class AppComponent  implements OnInit{
+  
+ 
+  clienteForm: FormGroup;
+
+
+
+
+  constructor(
+    public fb: FormBuilder,
+    public clienteService: ClienteService
+  ){
+
+  }
+  ngOnInit(): void {
+   
+    this.clienteForm = this.fb.group({
+
+    //  idUsuario : ['', Validators.required],
+      nombre : ['', Validators.required],
+      apellido : ['', Validators.required],
+      edad : ['', Validators.required],
+      documento : ['', Validators.required],
+      email : ['', Validators.required],
+      telefono : ['', Validators.required]
+    })
+
+  }
+
+  guardar():void{
+
+  }
+
 }
